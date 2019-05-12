ad2281ed35481c94979e07bd81277edcc82f2d3a
青木泰太郎 <aoksinn@aokiyasutarounoMac-mini.local>
2019-05-12 13:39:11 +0900

    layouts
    機能一覧ページ作成

---
+++ resources/views/Home/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h2>機能一覧</h2>
+    <div class="list-group">
+        <a href="#" class="list-group-item list-group-item-action">商品</a>
+        <a href="#" class="list-group-item list-group-item-action">取引先</a>
+        <a href="#" class="list-group-item list-group-item-action">項目定義</a>
+        <a href="#" class="list-group-item list-group-item-action">入庫登録</a>
+        <a href="#" class="list-group-item list-group-item-action">出庫登録</a>
+        <a href="#" class="list-group-item list-group-item-action">入出庫一覧</a>
+        <a href="#" class="list-group-item list-group-item-action">入出庫推移表</a>
+    </div>
+</div>
+@endsection
