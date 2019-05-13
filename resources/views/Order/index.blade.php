af010c4980f64d9ed9fa678de71739cfb5ae251f
青木泰太郎 <aoksinn@aokiyasutarounoMac-mini.local>
2019-05-13 21:43:22 +0900

    package update

--- resources/views/Order/index.blade.php
+++ resources/views/Order/index.blade.php
@@ -12,60 +12,73 @@
             {{session('success_message')}}
         </div>
     @endif
-
-    <h2>注文一覧</h2>
-    <div class="lcol text-right mb-3">
-        <a href="{{ action('HomeController@index') }}" class="btn btn-primary btn-lg" >戻る</a>
-        <a href="{{ action('ProductsController@new') }}" class="btn btn-primary btn-lg" >新規登録</a>
-    </div>
-    <div class="row justify-content-center">
-        <div class="table table-striped table-hover">
-            <table class="table table-striped table-bordered table-hover table-condensed">
-                <tr>
-                    <th>ID</th>
-                    <th>type</th>
-                    <th>日付</th>
-                    <th>注文名</th>
-                    <th>操作</th>
-                </tr>
-                @forelse ($itmes as $item )
-                <tr>
-                    <th>
-                        {{ $item->id }}
-                    </th>
-                    <th>
-                        {{ $item->code }}
-                    </th>
-                    <th>
-                        {{ $const['PRODUCT_CATEGORY'][$item->category] }}
-                    </th>
-                   <th>
-                        {{ $item->name }}/{{ $item->abbreviation }}
-                    </th>
-                    <th class="text-right">
-                        {{ $const['UNIT'][$item->unit] }}
-                    </th>
-                    <th class="text-right">
-                        {{$item->unit_selling_price}} 円
-                    </th>
-                    <th class="text-right">
-                        {{$item->unit_price}}円
-                    </th>
-                    <th>
-                        <a href="{{ action('ProductsController@edit',[ 'id' => $item->id]) }}" class="btn btn-primary btn-lg" >編集</a>
-                    </th>
-                </tr>
-                @empty
-                    <tr align="center">
-                        <th colspan="8">注文がありません</th>
+    <div id = 'component1'>
+        <h2>注文一覧</h2>
+        <div class="lcol text-right mb-3">
+            <a href="{{ action('HomeController@index') }}" class="btn btn-primary btn-lg" >戻る</a>
+            <a href="{{ action('ProductsController@new') }}" class="btn btn-primary btn-lg" >新規登録</a>
+        </div>
+        <div class="row justify-content-center">
+            <div class="table table-striped table-hover">
+                <table class="table table-striped table-bordered table-hover table-condensed">
+                    <tr>
+                        <th>ID</th>
+                        <th>type</th>
+                        <th>日付</th>
+                        <th>注文名</th>
+                        <th>操作</th>
                     </tr>
-                @endforelse
-            </table>
+                    @forelse ($itmes as $item )
+                    <tr>
+                        <th>
+                            {{ $item->id }}
+                        </th>
+                        <th>
+                            {{ $item->code }}
+                        </th>
+                        <th>
+                            {{ $const['PRODUCT_CATEGORY'][$item->category] }}
+                        </th>
+                    <th>
+                            {{ $item->name }}/{{ $item->abbreviation }}
+                        </th>
+                        <th class="text-right">
+                            {{ $const['UNIT'][$item->unit] }}
+                        </th>
+                        <th class="text-right">
+                            {{$item->unit_selling_price}} 円
+                        </th>
+                        <th class="text-right">
+                            {{$item->unit_price}}円
+                        </th>
+                        <th>
+                            <a href="{{ action('ProductsController@edit',[ 'id' => $item->id]) }}" class="btn btn-primary btn-lg" >編集</a>
+                        </th>
+                    </tr>
+                    @empty
+                        <tr align="center">
+                            <th colspan="8">注文がありません</th>
+                        </tr>
+                    @endforelse
+                </table>
+            </div>
+        </div>
+        <div class="lcol text-right mt-3" >
+            <a href="{{ action('HomeController@index') }}" class="btn btn-primary btn-lg" >戻る</a>
+            <a href="{{ action('ProductsController@new') }}" class="btn btn-primary btn-lg" >新規登録</a>
         </div>
     </div>
-    <div class="lcol text-right mt-3" >
-        <a href="{{ action('HomeController@index') }}" class="btn btn-primary btn-lg" >戻る</a>
-        <a href="{{ action('ProductsController@new') }}" class="btn btn-primary btn-lg" >新規登録</a>
-    </div>
 </div>
+
+@endsection
+@section('script')
+
+<script>
+$(function(){
+    $('#btn').click(function(){
+
+        $('#modal-xl').modal('show');
+    })
+});
+</script>
 @endsection
---
+++ resources/views/layouts/modal.blade.php
@@ -0,0 +1,15 @@
+<div class="modal fade" id="modal-xl" tabindex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" style="display: none;" aria-hidden="true">
+    <div class="modal-dialog modal-xl" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="myExtraLargeModalLabel">モーダルのタイトル</h5>
+                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">閉じる</span></button>
+            </div>
+            <div class="modal-body"><p>モーダルのコンテンツ</p></div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">閉じる</button>
+                <button type="button" class="btn btn-primary">変更を保存</button>
+            </div>
+        </div>
+    </div>
+</div>
