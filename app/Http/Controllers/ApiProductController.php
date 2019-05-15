72f9720c28f9f292d465e028a6b4ba37a94b32fa
青木泰太郎 <aoksinn@aokiyasutarounoMac-mini.local>
2019-05-16 00:22:02 +0900

    api add

---
+++ app/Http/Controllers/ApiProductController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use Illuminate\Http\Request;
+
+class ApiProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Product::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        //
+        return $product;
+    }
+
+    public function code_show(Request $request)
+    {
+        $code = $request->code;
+        $target = Product::where('code','=',$code)->first();
+
+        if (isset($target)) {
+            return response($target, 200)->header('Content-Type', 'application/json; charset=utf-8');
+        } else {
+            return response('Not Found', 404)->header('Content-Type', 'application/json');
+        }
+    }
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('product', 'ApiProductController');
+Route::get('/product/code/show', 'ApiProductController@code_show');
